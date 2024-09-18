1c5db9d63805af2122ee4bf620d34379578866a3
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-09-18 18:31:22 -0400

    Add tenant_user table and config setting
    Introduced the creation of a tenant_user table in the migration file to handle user-tenant relationships with foreign key constraints and cascade delete actions. Also, added the table_relation_name configuration setting to facilitate this new table name.

--- config/filament-tenant.php
+++ config/filament-tenant.php
@@ -3,5 +3,7 @@
 // config for Devlense/FilamentTenant
 return [
     'table_name' => 'tenants',
+    'table_relation_name' => 'tenant_user',
     'model' => Devlense\FilamentTenant\Models\Tenant::class,
+
 ];
