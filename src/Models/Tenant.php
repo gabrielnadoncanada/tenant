592624f445d8f77c8f157d34dac2df86f866d3ba
gabrielnadoncanada <gabrielnadoncanada@users.noreply.github.com>
2024-09-19 00:18:31 +0000

    Fix styling

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -3,10 +3,8 @@
 namespace Devlense\FilamentTenant\Models;
 
 use Filament\Models\Contracts\HasName;
-use App\Models\Users;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model implements HasName
@@ -23,8 +21,6 @@ class Tenant extends Model implements HasName
     public const CREATED_AT = 'created_at';
 
     public const UPDATED_AT = 'updated_at';
-
-
 
     public function getFilamentName(): string
     {
