8f1bb3e60d420c8752e194f86483e47aa620f14d
Gabriel Nadon <gabrielnadoncanada@gmail.com>
2024-09-19 10:43:29 -0400

    Refactor tenant handling logic
    Reorganize tenant-related methods and streamline definitions in `FilamentTenant.php`. Rename `HasTenant.php` to

---
+++ src/Concerns/Multitenancy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Devlense\FilamentTenant\Concerns;
+
+use Filament\Facades\Filament;
+
+trait Multitenancy
+{
+
+
+    protected static function bootHasMultiTenancy()
+    {
+        static::creating(function ($model) {
+            $tenant = Filament::getTenant();
+
+            if (is_null($model->{static::getTenantForeignKey()}) && $tenant !== null) {
+                $model->{static::getTenantForeignKey()} = $tenant->id;
+            }
+        });
+
+
+        if (Filament::getTenant() != null) {
+            static::addGlobalScope('by_tenant', function (Builder $builder) {
+                $builder->where(static::getTenantForeignKey(), Filament::getTenant()->id);
+            });
+        }
+    }
+}
--- src/FilamentTenant.php
+++ src/FilamentTenant.php
@@ -3,30 +3,13 @@
 namespace Devlense\FilamentTenant;
 
 class FilamentTenant {
-
-    /**
-     * Get the column name that references the tenant in the model.
-     */
-    public static function getTenantForeignKey(): string
-    {
-        return config('filament-tenant.relation_foreign_key');
-    }
-
-    /**
-     * Get the tenant model class.
-     *
-     * @return string
-     */
     public static function getTenantModelClass()
     {
         return config('filament-tenant.model', 'model');
     }
 
-    /**
-     * Get the current tenant instance from Filament.
-     */
-    public static function getCurrentTenant(): mixed
+    public static function getTenantForeignKey(): string
     {
-        return \Filament\Facades\Filament::getTenant();
+        return config('filament-tenant.relation_foreign_key');
     }
 }
